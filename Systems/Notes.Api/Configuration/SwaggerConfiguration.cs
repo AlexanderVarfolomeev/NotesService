53632ca34d554df10a26d76d549f1e5c88c7ca4b
NikolnikovaPolina <alexander.varfolomei@gmail.com>
2022-07-18 17:30:00 +0300

    Add swagger and serilog for API

---
+++ Systems/Notes.Api/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Notes.Api.Configuration;
+
+public static class SwaggerConfiguration
+{
+    public static IServiceCollection AddAppSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static IApplicationBuilder UseAppSwagger(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        return app;
+    }
+}
--- Systems/Notes.Api/Program.cs
+++ Systems/Notes.Api/Program.cs
@@ -1,15 +1,28 @@
+using Notes.Api.Configuration;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-// Add services to the container.
+// read configuration for serilog
+builder.Host.UseSerilog((host, cfg) =>
+{
+    cfg.ReadFrom.Configuration(host.Configuration);
+});
 
-builder.Services.AddControllers();
+
+services.AddControllers();
+
+services.AddAppSwagger();
+
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseAppSwagger();
 
 app.Run();
