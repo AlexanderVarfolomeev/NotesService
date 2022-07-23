cc5f03b81039544367c46273630a3b7952e5d820
Alexander Varfolomeev <alexander.varfolomei@gmail.com>
2022-07-23 10:50:34 +0300

    Add edit task type dialog

--- Systems/Notes.WEB/Pages/TaskTypes/Models/TaskType.cs
+++ Systems/Notes.WEB/Pages/TaskTypes/Models/TaskType.cs
@@ -6,6 +6,7 @@ public class TaskType
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public int TypeColorId { get; set; }
     public TypeColor Color { get; set; }
 }
 
