5bbd5469e1d7b0b9c4b88f5c809879c6880da1fd
Alexander Varfolomeev <alexander.varfolomei@gmail.com>
2022-07-26 13:35:41 +0300

    Add db seed

--- Data/Notes.Context/Setup/DbSeed.cs
+++ Data/Notes.Context/Setup/DbSeed.cs
@@ -16,57 +16,171 @@ public class DbSeed
         var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<MainDbContext>>();
         using var context = factory.CreateDbContext();
 
-        //AddData(context);
+        AddColors(context);
+        AddTaskTypes(context);
+        AddNotes(context);
     }
 
-    //private static void AddData(MainDbContext context)
-    //{
-    //    if (context.Tasks.Any() || context.Notes.Any())
-    //        return;
+    private static void AddColors(MainDbContext context)
+    {
+        if (context.Colors.Any())
+            return;
 
-    //    var type1 = new TaskType()
-    //    {
-    //        Name = "Дом",
-    //        Color = ColorTaskType.Green
-    //    };
+        var c1 = new TypeColor()
+        {
+            Code = "#FF0000",
+            Name = "Red",
+        };
 
-    //    context.Tasks.Add(type1);
+        context.Colors.Add(c1);
 
-    //    var type2 = new TaskType()
-    //    {
-    //        Name = "Учеба",
-    //        Color = ColorTaskType.Red
-    //    };
+        var c2 = new TypeColor()
+        {
+            Code = "#00FFFF",
+            Name = "Aqua",
+        };
 
-    //    context.Tasks.Add(type2);
+        context.Colors.Add(c2);
 
-    //    var note1 = new Note()
-    //    {
-    //        Name = "Уборка",
-    //        Description = "Вымыть пол",
-    //        Status = TaskStatus.Waiting,
-    //        StartDateTime = new DateTime(2022, 7, 10, 15, 30, 0),
-    //        EndDateTime = new DateTime(2022, 7, 10, 16, 30, 0),
-    //        RepeatFrequency = RepeatFrequency.Weekly,
-    //        TaskTypeId = 1
-    //    };
+        var c3 = new TypeColor()
+        {
+            Code = "#00FF00",
+            Name = "Lime",
+        };
 
-    //    context.Notes.Add(note1);
+        context.Colors.Add(c3);
 
-    //    var note2 = new Note()
-    //    {
-    //        Name = "Работа",
-    //        Description = "",
-    //        Status = TaskStatus.Waiting,
-    //        StartDateTime = new DateTime(2022, 7, 10, 15, 30, 0),
-    //        EndDateTime = new DateTime(2022, 7, 10, 16, 30, 0),
-    //        RepeatFrequency = RepeatFrequency.Daily,
-    //        TaskTypeId = 2
-    //    };
+        var c4 = new TypeColor()
+        {
+            Code = "#FFC0CB",
+            Name = "Pink",
+        };
 
-    //    context.Notes.Add(note2);
+        context.Colors.Add(c4);
 
-    //    context.SaveChanges();
+        var c5 = new TypeColor()
+        {
+            Code = "#FFA500",
+            Name = "Orange",
+        };
 
-    //}
+        context.Colors.Add(c5);
+
+        var c6 = new TypeColor()
+        {
+            Code = "#FFFF00",
+            Name = "Yellow",
+        };
+
+        context.Colors.Add(c6);
+
+        var c7 = new TypeColor()
+        {
+            Code = "#C0C0C0",
+            Name = "Gray",
+        };
+
+        context.Colors.Add(c7);
+
+        var c8 = new TypeColor()
+        {
+            Code = "#C71585",
+            Name = "Violet",
+        };
+
+        context.Colors.Add(c8);
+        context.SaveChanges();
+
+    }
+
+    private static void AddTaskTypes(MainDbContext context)
+    {
+        if (context.Tasks.Any())
+            return;
+
+        var t1 = new TaskType()
+        {
+            Name = "Дом",
+            TypeColorId = 1
+        };
+
+        context.Tasks.Add(t1);
+
+        var t2 = new TaskType()
+        {
+            Name = "Работа",
+            TypeColorId = 2
+        };
+
+        context.Tasks.Add(t2);
+
+        var t3 = new TaskType()
+        {
+            Name = "Учеба",
+            TypeColorId = 3
+        };
+
+        context.Tasks.Add(t3);
+
+        var t4 = new TaskType()
+        {
+            Name = "Срочно",
+            TypeColorId = 4
+        };
+
+        context.Tasks.Add(t4);
+
+        var t5 = new TaskType()
+        {
+            Name = "Неважно",
+            TypeColorId = 5
+        };
+
+        context.Tasks.Add(t5);
+
+        context.SaveChanges();
+    }
+
+    private static void AddNotes(MainDbContext context)
+    {
+        if (context.Notes.Any())
+            return;
+
+        var n1 = new Note()
+        {
+            Name = "Помыть пол",
+            StartDateTime = new DateTimeOffset(2022, 7, 22, 13, 0, 0, TimeSpan.Zero),
+            EndDateTime = new DateTimeOffset(2022, 7, 22, 13, 30, 0, TimeSpan.Zero),
+            RepeatFrequency = RepeatFrequency.Weekly,
+            Status = TaskStatus.Failed,
+            TaskTypeId = 1
+        };
+
+        context.Notes.Add(n1);
+
+        var n2 = new Note()
+        {
+            Name = "Добавить кнопку на форму",
+            StartDateTime = new DateTimeOffset(2022, 7, 28, 15, 0, 0, TimeSpan.Zero),
+            EndDateTime = new DateTimeOffset(2022, 7, 28, 16, 30, 0, TimeSpan.Zero),
+            RepeatFrequency = RepeatFrequency.None,
+            Status = TaskStatus.Waiting,
+            TaskTypeId = 1
+        };
+
+        context.Notes.Add(n2);
+
+        var n3 = new Note()
+        {
+            Name = "Позвонить родителям",
+            StartDateTime = new DateTimeOffset(2022, 7, 28, 20, 0, 0, TimeSpan.Zero),
+            EndDateTime = new DateTimeOffset(2022, 7, 28, 20, 15, 0, TimeSpan.Zero),
+            RepeatFrequency = RepeatFrequency.Daily,
+            Status = TaskStatus.Waiting,
+            TaskTypeId = 4
+        };
+
+        context.Notes.Add(n3);
+        context.SaveChanges();
+    }
 }
